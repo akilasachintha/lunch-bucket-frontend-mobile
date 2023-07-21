e8c44e82c6abd406378d548ad6d209c83d78ce00
Akila Sachintha <akilasachintha@gmail.com>
2023-07-21 23:45:20 +0530

    remove gesture

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import {NavigationContainer} from "@react-navigation/native";
 import StackNavigator from "./src/navigation/StackNavigator";
 import {ToastProvider} from "./src/helpers/toast/Toast";
-import {GestureHandlerRootView} from "react-native-gesture-handler";
-import {StyleSheet} from "react-native";
 
 export default function App() {
     return (
-        <GestureHandlerRootView style={styles.container}>
-            <ToastProvider>
-                <NavigationContainer>
-                    <StackNavigator/>
-                </NavigationContainer>
-            </ToastProvider>
-        </GestureHandlerRootView>
+        <ToastProvider>
+            <NavigationContainer>
+                <StackNavigator/>
+            </NavigationContainer>
+        </ToastProvider>
     );
 }
 
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
-});
